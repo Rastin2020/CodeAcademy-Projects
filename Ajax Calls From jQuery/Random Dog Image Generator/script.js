683954defb0e01adfea1c9ba564d2cd856f24f1e
Rastin <56872628+Rastin2020@users.noreply.github.com>
2020-03-27 12:35:03 +0000

    Update script.js

--- Ajax Calls From jQuery/Random Dog Image Generator/script.js
+++ Ajax Calls From jQuery/Random Dog Image Generator/script.js
@@ -1,11 +1,12 @@
-// The following method can be used for JSON file apis only (url ending in ".json"):
 $(document).ready(function () {
 
     $('#ajaxBtn').click(function () {
         $.ajax('https://random.dog/woof.json',   // request url
             {
                 success: function (data, status, xhr) {    // success callback function
-                    $('img').attr("src", data.url);
+                    if (data.url.endsWith(".png") || data.url.endsWith(".jpg") || data.url.endsWith(".jpeg") || data.url.endsWith(".JPG")) {
+                        $("img").attr("src", data.url);
+                    }
                 }
             });
     });
